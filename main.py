3b0a5470d25bdc6623dcd1aab965d389a83333a2
Bodhi Satwa Mishra <60205891+mbodhisatwa@users.noreply.github.com>
2022-03-26 15:06:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,35 @@ import sys
 import argparse
 
 
-def convert_from_h5(input_file, output_format):
-    return
-
+def convert_from_hdf5(input_file, output_format):
+    if output_format == 'nc'
+        inpo = infput_file
+        sys_inp = r'ncks '
+        sys_inp += inpo
+        sys_inp += r'output.nc'
+        os.system(sys_inp)
+    return 
 
 def convert_from_netcdf(input_file, output_format):
-    return
+    if output_format=='asci':  
+        var=input()
+        system_input=r'ncdump -b c '
+        system_input+=var
+        system_input+=r' > '
+        system_input+=r'output.asci'
+        os.system(system_input)
+    return 
 
 
 if __name__ == '__main__':
     cliParser = argparse.ArgumentParser(description="Radar file conversion tool")
 
     cliParser.add_argument('Path', metavar='path', type=str, help='path to input file')
-    cliParser.add_argument('Output', choices=['h5', 'nc', 'ascii', 'bufr', 'iris'])
+    cliParser.add_argument('Output', choices=['hdf5', 'nc', 'asci', 'bufr', 'iris'])
 
     args = cliParser.parse_args()
 
-    allowedInputFormats = {'h5', 'nc'}
+    allowedInputFormats = {'hdf5', 'nc'}
     inputFile = args.Path
     inputFormat = inputFile.split('.')[-1]
     outputFormat = args.Output
@@ -37,7 +49,7 @@ if __name__ == '__main__':
         print("Input and output format are the same")
         sys.exit()
 
-    if inputFormat == 'h5':
-        convert_from_h5(inputFile, outputFormat)
+    if inputFormat == 'hdf5':
+        convert_from_hdf5(inputFile, outputFormat)
     elif inputFile == 'nc':
         convert_from_netcdf(inputFile, outputFormat)
