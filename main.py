738a180c6336c82ae21e24141bedfdf3ee2a4fbe
aryan <aryannarayans@gmail.com>
2022-03-26 10:33:41 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+import argparse
+
+
+def convert_from_h5(input_file, output_format):
+    return
+
+
+def convert_from_netcdf(input_file, output_format):
+    return
+
+
+if __name__ == '__main__':
+    cliParser = argparse.ArgumentParser(description="Radar file conversion tool")
+
+    cliParser.add_argument('Path', metavar='path', type=str, help='path to input file')
+    cliParser.add_argument('Output', choices=['hdf5', 'netcdf', 'ascii', 'bufr', 'iris'])
+
+    args = cliParser.parse_args()
+
+    allowedInputFormats = {'hdf5', 'netcdf'}
+    inputFile = args.Path
+    inputFormat = inputFile.split('.')[-1]
+    outputFormat = args.Output
+
+    if not os.path.exists(inputFile):
+        print('The file specified does not exist')
+        sys.exit()
+
+    if inputFormat not in allowedInputFormats:
+        print("Can't convert the given file type")
+        sys.exit()
+
+    if inputFormat == outputFormat:
+        print("Input and output format are the same")
+        sys.exit()
+
+    if inputFormat == 'hdf5':
+        convert_from_h5(inputFile, outputFormat)
+    elif inputFile == 'netcdf':
+        convert_from_netcdf(inputFile, outputFormat)
