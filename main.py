68bb843ee871594201be80030172ff466e8949d3
aryan <aryannarayans@gmail.com>
2022-03-26 16:23:34 +0530

    Change cli to gui

--- main.py
+++ main.py
@@ -1,43 +1,74 @@
+#!/usr/bin/python3
+
 import os
 import sys
-
-import argparse
-
-
-def convert_from_h5(input_file, output_format):
-    return
+from tkinter import *
+from tkinter import filedialog
 
 
-def convert_from_netcdf(input_file, output_format):
-    return
+class RadarConversion():
+    def __init__(self, master):
+        self.nc_to_ascii = None
+        self.h5_to_ascii = None
+        self.nc_to_h5 = None
+        self.h5_to_nc = None
+        self.button = None
+        self.label = None
+        self.selected_file = None
+        self.selected_file_format = None
+        self.master = master
+        master.title("Radar Conversion")
+        master.geometry("400x200")
+        master.resizable(False, False)
+        self.create_widgets()
+        self.pack_widgets()
+
+    def create_widgets(self):
+        self.label = Label(self.master, text="Select a file to convert")
+        self.button = Button(self.master, text="Select File", command=self.select_file)
+        self.h5_to_nc = Button(self.master, text="Convert", command=self.convert_to_nc)
+        self.h5_to_ascii = Button(self.master, text="Convert to ASCII", command=self.convert_to_ascii)
+        self.nc_to_h5 = Button(self.master, text="Convert to HDF5", command=self.convert_to_h5)
+        self.nc_to_ascii = Button(self.master, text="Convert to ASCII", command=self.convert_to_ascii)
+
+    def pack_widgets(self):
+        self.label.pack(padx=10, pady=10)
+        self.button.pack()
+
+    def select_file(self):
+        self.selected_file = filedialog.askopenfilename(initialdir="~", title="Select file",
+                                                        filetypes=(("HDF5", "*.h5"), ("NetCDF", "*.nc")))
+        if self.selected_file:
+            self.selected_file_format = self.selected_file.split(".")[-1]
+            label = Label(self.master, text='Selected File: ' + self.selected_file)
+            label.pack(padx=10, pady=10)
+            if self.selected_file_format == "h5":
+                self.h5_to_nc.pack()
+                self.h5_to_ascii.pack()
+            elif self.selected_file_format == "nc":
+                self.nc_to_h5.pack()
+                self.nc_to_ascii.pack()
+            else:
+                print("File format not supported")
+
+    def convert_to_nc(self):
+        sys_inp = r'ncks '
+        sys_inp += self.selected_file
+        sys_inp += r'output.nc'
+        os.system(sys_inp)
+
+    def convert_to_ascii(self):
+        if self.selected_file_format == 'nc':
+            system_input = r'ncdump -b c '
+            system_input += self.selected_file
+            system_input += r' > '
+            system_input += r'output.cdl'
+            os.system(system_input)
+
+    def convert_to_h5(self):
+        pass
 
 
-if __name__ == '__main__':
-    cliParser = argparse.ArgumentParser(description="Radar file conversion tool")
-
-    cliParser.add_argument('Path', metavar='path', type=str, help='path to input file')
-    cliParser.add_argument('Output', choices=['h5', 'nc', 'ascii', 'bufr', 'iris'])
-
-    args = cliParser.parse_args()
-
-    allowedInputFormats = {'h5', 'nc'}
-    inputFile = args.Path
-    inputFormat = inputFile.split('.')[-1]
-    outputFormat = args.Output
-
-    if not os.path.exists(inputFile):
-        print('The file specified does not exist')
-        sys.exit()
-
-    if inputFormat not in allowedInputFormats:
-        print("Can't convert the given file type")
-        sys.exit()
-
-    if inputFormat == outputFormat:
-        print("Input and output format are the same")
-        sys.exit()
-
-    if inputFormat == 'h5':
-        convert_from_h5(inputFile, outputFormat)
-    elif inputFile == 'nc':
-        convert_from_netcdf(inputFile, outputFormat)
+root = Tk()
+app = RadarConversion(root)
+root.mainloop()
